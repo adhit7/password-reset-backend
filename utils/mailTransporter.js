5dd19b5f9f74cf05556a30d60318573aa9a0516f
adhit7 <adhithyanalan@gmail.com>
2024-03-21 09:21:11 +0530

    minor changes in backend

--- utils/mailTransporter.js
+++ utils/mailTransporter.js
@@ -5,7 +5,7 @@ const mailTransporter = () => {
     service: 'gmail',
     host: 'smtp.gmail.com',
     port: 587,
-    secure: false,
+    secure: process.env.NODE_ENV !== 'production',
     auth: {
       user: 'adhithyanalan@gmail.com',
       pass: process.env.MAILER_PASS,
